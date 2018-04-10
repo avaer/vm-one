b6989485f1d3910faff317332ece3f2d98825f78
Avaer Kazmer <a@modules.io>
2018-04-10 05:13:27 -0400

    Re-use global init objects

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const {VmOne} = require('./build/Release/vmOne.node');
 
+const vmSymbol = Symbol();
+
 let compiling = false;
 function vmOne(jsString = '', globalInit = {}, fileName = 'script', lineOffset = 0, colOffset = 0) {
-  const vm = new VmOne(globalInit, e => {
-    if (e === 'compilestart') {
-      compiling = true;
-    } else if (e === 'compileend') {
-      compiling = false;
-    }
-  });
+  let vm = globalInit[vmSymbol];
+  if (!vm) {
+    vm = new VmOne(globalInit, e => {
+      if (e === 'compilestart') {
+        compiling = true;
+      } else if (e === 'compileend') {
+        compiling = false;
+      }
+    });
+    globalInit[vmSymbol] = vm;
+  }
+
   return vm.run(jsString, fileName, lineOffset, colOffset);
 }
 vmOne.isCompiling = () => compiling;
