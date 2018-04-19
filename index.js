1956db97c9a11531f639ddaf01024cd061c640bc
Avaer Kazmer <a@modules.io>
2018-04-19 00:49:18 -0400

    Refactor top-level api

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 const {VmOne} = require('./build/Release/vmOne.node');
 
-const vmSymbol = Symbol();
-
 let compiling = false;
-function vmOne(jsString = '', globalInit = {}, fileName = 'script', lineOffset = 0, colOffset = 0) {
-  let vm = globalInit[vmSymbol];
-  if (!vm) {
-    vm = new VmOne(globalInit, e => {
-      if (e === 'compilestart') {
-        compiling = true;
-      } else if (e === 'compileend') {
-        compiling = false;
-      }
-    });
-    globalInit[vmSymbol] = vm;
+const make = (globalInit = {}) => new VmOne(globalInit, e => {
+  if (e === 'compilestart') {
+    compiling = true;
+  } else if (e === 'compileend') {
+    compiling = false;
   }
+});
+const isCompiling = () => compiling;
 
-  return vm.run(jsString, fileName, lineOffset, colOffset);
+const vmOne = {
+  make,
+  isCompiling,
 }
-vmOne.isCompiling = () => compiling;
 
 module.exports = vmOne;
