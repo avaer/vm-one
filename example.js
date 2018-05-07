162ce6b75e70ca746cfd4b806d1d0ec8076f577b
Avaer Kazmer <a@modules.io>
2018-05-07 11:37:53 -0400

    Update example.js

--- example.js
+++ example.js
@@ -1,6 +1,20 @@
+const fs = require('fs');
+fs.tainted = true;
+
 const vmOne = require('.');
 
-vmOne(`console.log(lol);`, {
-  console,
-  lol: 'zol',
-});
+const v = vmOne.make();
+const g = v.getGlobal();
+g.lol = 'zol';
+g.callback = object => {
+  console.log('check 3', object, !(object instanceof Object));
+};
+
+const result = v.run(`
+  const fs = require('fs');
+  console.log('check 0', lol === 'zol');
+  console.log('check 1', fs.tainted === undefined);
+  console.log('check 2', fs.readFileSync('./boot.js').buffer instanceof ArrayBuffer);
+  callback({});
+`);
+
