48aabf6a6aeca51638fdfa576a6764fb217c02e1
Shawn Presser <shawnpresser@gmail.com>
2018-06-21 17:34:04 -0500

    Support zeit/pkg

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
 const path = require('path');
 const {VmOne, setPrototype} = require('./build/Release/vmOne.node');
 
-let compiling = false;
-const make = (globalInit = {}) => new VmOne(globalInit, e => {
-  if (e === 'compilestart') {
-    compiling = true;
-  } else if (e === 'compileend') {
-    compiling = false;
+const pushEnv = (name, value, cb) => {
+  const del = !process.env.hasOwnProperty(name);
+  const prev = process.env[name];
+  process.env[name] = value;
+  const result = cb();
+  if (del) {
+    delete process.env[name];
+  } else {
+    process.env[name] = prev;
   }
-}, __dirname + path.sep);
+  return result;
+}
+
+let compiling = false;
+const make = (globalInit = {}) => pushEnv('PKG_EXECPATH', 'PKG_INVOKE_NODEJS',
+  () => new VmOne(globalInit, e => {
+    if (e === 'compilestart') {
+      compiling = true;
+    } else if (e === 'compileend') {
+      compiling = false;
+    }
+  }, (process.pkg ? path.dirname(process.execPath) : __dirname) + path.sep))
+
 const isCompiling = () => compiling;
 
 const vmOne = {
