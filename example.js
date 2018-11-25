e80856b1960adb797f854deffa8ebd4513a36043
Avaer Kazmer <a@modules.io>
2018-11-25 16:43:27 -0500

    Add postMessage testing to example

--- example.js
+++ example.js
@@ -5,7 +5,12 @@ const vmOne = require('.');
     const v = vmOne.make({
       initModule: './example-module.js',
     });
+    v.on('message', m => {
+      console.log('parent got message', m);
+    });
+
     console.log('example 1');
+
     const int32Array = Int32Array.from([1, 2, 3]);
     const int32SharedArray = new Int32Array(new SharedArrayBuffer(3 * Int32Array.BYTES_PER_ELEMENT));
     int32SharedArray[0] = 7;
@@ -13,33 +18,50 @@ const vmOne = require('.');
       console.log('example 2', arg.int32Array, arg.int32SharedArray);
 
       global.lol = 'zol';
+      global.on('message', m => {
+        console.log('child got message', m);
+
+        global.postMessage(m);
+      });
       return 'woot';
     `, {
       int32Array,
       int32SharedArray,
     }, [int32Array.buffer]);
+
     console.log('example 3', result);
+
+    v.postMessage({
+      lol: 'zol',
+    });
+
+    console.log('example 4');
+
     result = await v.runAsync(`
-      console.log('example 4', arg.int32SharedArray);
+      console.log('example 5', arg.int32SharedArray);
 
       global.lol = 'zol2';
       return 'toot';
     `, {
       int32SharedArray,
     });
-    console.log('example 5', result);
+
+    console.log('example 6', result);
   }
 
   {
     const v = vmOne.make();
-    console.log('example 6');
+
+    console.log('example 7');
+
     let result = v.runSync(`
-      console.log('example 7');
+      console.log('example 8');
 
       global.lol = 'zol';
       return 'woot';
     `);
-    console.log('example 8', result);
+
+    console.log('example 9', result);
   }
 
   setTimeout(() => {
