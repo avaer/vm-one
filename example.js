0c22974d168cc1353916fde25592b738bcf0d378
Avaer Kazmer <a@modules.io>
2018-11-25 13:26:59 -0500

    Test argument and transfer list in example

--- example.js
+++ example.js
@@ -4,19 +4,27 @@ const vmOne = require('.');
   {
     const v = vmOne.make();
     console.log('example 1');
+    const int32Array = Int32Array.from([1, 2, 3]);
+    const int32SharedArray = new Int32Array(new SharedArrayBuffer(3 * Int32Array.BYTES_PER_ELEMENT));
+    int32SharedArray[0] = 7;
     let result = v.runSync(`
-      console.log('example 2');
+      console.log('example 2', arg.int32Array, arg.int32SharedArray);
 
       global.lol = 'zol';
       return 'woot';
-    `);
+    `, {
+      int32Array,
+      int32SharedArray,
+    }, [int32Array.buffer]);
     console.log('example 3', result);
     result = await v.runAsync(`
-      console.log('example 4');
+      console.log('example 4', arg.int32SharedArray);
 
       global.lol = 'zol2';
       return 'toot';
-    `);
+    `, {
+      int32SharedArray,
+    });
     console.log('example 5', result);
   }
 
