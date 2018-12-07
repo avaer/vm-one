a1926c1109a5fb1c279e96df1bd5c2ef73a76ff0
Avaer Kazmer <a@modules.io>
2018-12-06 19:33:07 -0500

    Add importScripts test

--- example-module.js
+++ example-module.js
@@ -3,3 +3,7 @@ console.log('example module 1');
 const vmOne = requireNative('vm-one');
 
 console.log('example module 2', typeof vmOne);
+
+importScripts('file://./example-import.js');
+
+console.log('example module 3');
